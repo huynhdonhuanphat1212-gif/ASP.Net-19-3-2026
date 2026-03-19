fa42f5408b00639b070acce1818f9aee5928eb3c
huynhdonhuanphat1212-gif <huynhdonhuanphat1212@gmail.com>
2026-03-19 16:10:20 +0700

    Bài tập

---
+++ demo/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using demo.Models;
+
+namespace demo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        // Constructor bắt buộc
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Bảng Students
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ demo/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using demo.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// 👉 Kết nối SQL Server
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
